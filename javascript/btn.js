88e341c16e00ee512e4b038645e6eb2f61be8acd
Dioni <Liusuga23@gmail.com>
2024-02-27 01:57:47 -0400

    first commit

---
+++ javascript/btn.js
@@ -0,0 +1,7 @@
+<script>
+var chatBtn = document.querySelector('.chat-btn');
+var chatBox = document.querySelector('.chat-box');
+
+chatBtn.addEventListener('click', function() {
+	chatBox.style.display = chatBox.style.display === 'block' ? 'none' : 'block'});
+</script>
